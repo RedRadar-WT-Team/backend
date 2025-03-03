caa47058857ec603e7f5ac49da8a57e60ba31027
Jonathan Atkins <jonathanjatkins89@gmail.com>
2025-03-02 18:25:07 -0800

    edit: giving access to Kristin W

--- app/poros/executive_order.rb
+++ app/poros/executive_order.rb
@@ -1,3 +1,11 @@
 class ExecutiveOrder
-  attr_reader : id, title, :url,
+  attr_reader :id, :title, :html_url, :document_number, :pdf_url, :publication_date
+
+  def initialize(order_data)
+    @title            = order_data[:title]
+    @document_number  = order_data[:document_number]
+    @html_url         = order_data[:html_url]
+    @pdf_url          = order_data[:pdf_url]
+    @publication_date = order_data[:publication_date] 
+  end
 end
--- spec/gateways/executive_order_gateway_spec.rb
+++ spec/gateways/executive_order_gateway_spec.rb
@@ -1,15 +1,14 @@
 require "rails_helper"
+# require '/app/gateways/executive_order_gateway'
+
+
 
 RSpec.describe ExecutiveOrderGateway do
-  it "should make a call to Federalist to retrieve 5 most recent Executive Orders",
+  it "should make a call to Federalist to retrieve 5 most recent Executive Orders" do
+    require 'pry'; binding.pry
     executive_orders = ExecutiveOrderGateway.five_most_recent
 
     expect(executive_orders[0]).not_to be_nil
-    expect(executive_orders[0]).to be_an_instance_of(ExecutiveOrder)    
-
-    # json_response.map do |movie|
-      expect(movie.title).to be_an(String)
-      expect(movie.vote_average).to be_an(Float)
-    # end
+    expect(executive_orders[0]).to be_an_instance_of(ExecutiveOrder)
   end
 end
