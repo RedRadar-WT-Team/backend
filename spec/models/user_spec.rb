785dc7ef6f13b275a9fc5218ab80f4004c37a459
Jillian Bleggi <jillian.bleggi@gmail.com>
2025-03-03 10:06:30 -0500

    Update Model spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,44 +1,49 @@
+# bundle exec spec/models/user_spec.rb
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it 'is valid with valid attributes' do
-    user = User.new(email: 'user@example.com', state: 'CA', zip: '90001')
-    expect(user).to be_valid
+  let(:valid_attributes) { { email: 'user@example.com', state: 'CA', zip: '90001' } }
+
+  describe 'validations' do
+    it 'is valid with valid attributes' do
+      user = User.new(valid_attributes)
+      expect(user).to be_valid
+    end
+
+    it 'is not valid without an email' do
+      user2 = User.new(email: '', state: 'CA', zip: '90001')
+      expect(user2).to_not be_valid
+      expect(user2.errors[:email]).to include("can't be blank")
+    end
+
+    it 'is not valid without a state' do
+      user3 = User.new(email: 'user3@example.com', state: '', zip: '90001')
+      expect(user3).to_not be_valid
+      expect(user3.errors[:state]).to include("can't be blank")
+    end
+
+    it 'is not valid without a zip' do
+      user4 = User.new(email: 'user4@example.com', state: 'CA', zip: '')
+      expect(user4).to_not be_valid
+      expect(user4.errors[:zip]).to include("can't be blank")
+    end
+
+    it 'is not valid with a duplicate email' do
+      User.create(valid_attributes)
+      user6 = User.new(valid_attributes)
+
+      expect(user6).to_not be_valid
+      expect(user6.errors[:email]).to include("has already been taken")
+    end
   end
 
-  it 'is invalid without an email' do
-    user = User.new(state: 'CA', zip: '90001')
-    user.valid? 
-    expect(user.errors[:email]).to include("can't be blank")
-  end
-
-  it 'is invalid without a state' do
-    user = User.new(email: 'user@example.com', zip: '90001')
-    user.valid?
-    expect(user.errors[:state]).to include("can't be blank")
-  end
-
-  it 'is invalid without a zip code' do
-    user = User.new(email: 'user@example.com', state: 'CA')
-    user.valid?
-    expect(user.errors[:zip]).to include("can't be blank")
-  end
-
-  it 'is invalid with an invalid email format' do
-    user = User.new(email: 'invalid_email', state: 'CA', zip: '90001')
-    user.valid?
-    expect(user.errors[:email]).to include('is invalid')
-  end
-
-  it "is invalid with an invalid zip code" do
-    user = User.new(email: "user@example.com", state: "CA", zip: "9000")
-    expect(user).to_not be_valid
-    expect(user.errors[:zip]).to include("must be a 5-digit zip code")
-  end
-
-  it "is invalid with a non-numeric zip code" do
-    user = User.new(email: "user@example.com", state: "CA", zip: "abcde")
-    expect(user).to_not be_valid
-    expect(user.errors[:zip]).to include("must be a 5-digit zip code")
+  describe 'creating a user' do
+    it 'creates a user with valid attributes' do
+      user = User.create(valid_attributes)
+      expect(user).to be_persisted
+      expect(user.email).to eq('user@example.com')
+      expect(user.state).to eq('CA')
+      expect(user.zip).to eq('90001')
+    end
   end
 end
