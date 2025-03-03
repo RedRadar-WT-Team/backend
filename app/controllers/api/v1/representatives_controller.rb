2513014c37984265eb82fc3f7b4f2d1f99c7265b
elysableu <elysabward@gmail.com>
2025-03-03 12:52:35 -0700

    Update representativesController with index endpoint and added to routes

--- app/controllers/api/v1/representatives_controller.rb
+++ app/controllers/api/v1/representatives_controller.rb
@@ -1,3 +1,6 @@
 class Api::V1::RepresentativesController < ApplicationController
-  
+  def index
+    representatives = RepresentativeGateway.fetch_queried_reps(params[:query])
+    render json: RepresentativeSerializer.new(representatives)
+  end
 end
