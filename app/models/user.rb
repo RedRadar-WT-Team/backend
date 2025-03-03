4074367daad00fd668b33b1283a7caf190d67809
Jillian Bleggi <jillian.bleggi@gmail.com>
2025-03-02 22:53:04 -0500

    Update Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 # app/models/user.rb
 
 class User < ApplicationRecord
-  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: "is invalid" }
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: "Email format is invalid" }
   validates :state, presence: true
-  validates :zip, presence: true, format: { with: /\A\d{5}\z/, message: "must be a 5-digit zip code" }
+  validates :zip, presence: true, format: { with: /\A\d{5}\z/, message: "Must be a valid 5-digit zip code" }
 end
