8932cef854d1f552f32ea3cfbf88a71d102e15db
Jillian Bleggi <jillian.bleggi@gmail.com>
2025-03-03 09:56:17 -0500

    Added uniqueness to email to avoid duplicate users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 # app/models/user.rb
 
 class User < ApplicationRecord
-  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: "Email format is invalid" }
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: "Email format is invalid" }, uniqueness: true
   validates :state, presence: true
   validates :zip, presence: true, format: { with: /\A\d{5}\z/, message: "Must be a valid 5-digit zip code" }
 end
