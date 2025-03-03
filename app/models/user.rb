50ba7632b5621b9558947d42d1a4e73c83103830
Jillian Bleggi <jillian.bleggi@gmail.com>
2025-03-02 21:28:57 -0500

    Added regex for zipcode validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@
 class User < ApplicationRecord
   validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: "is invalid" }
   validates :state, presence: true
-  validates :zip, presence: true
+  validates :zip, presence: true, format: { with: /\A\d{5}\z/, message: "must be a 5-digit zip code" }
 end
