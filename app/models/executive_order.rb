3102501c7dff47959321fd7d7107a509af097287
Kevin Newland <kevin.newland@yahoo.com>
2025-03-05 11:30:22 -0600

    created model for executive order

---
+++ app/models/executive_order.rb
@@ -0,0 +1,9 @@
+class ExecutiveOrder < ApplicationRecord
+  validates :title, presence: true
+  validates :html_url, presence: true
+  validates :executive_order_number, presence: true
+  validates :publication_date, presence: true
+ 
+  has_many :executive_orders_users
+  has many :users, through: :executive_orders_users
+end
