35169d92082c78b3a65d17972c88526d6a6106cb
Kevin Newland <kevin.newland@yahoo.com>
2025-03-05 12:34:58 -0600

    progress on the create action

--- app/controllers/api/v1/executive_orders_users_controller.rb
+++ app/controllers/api/v1/executive_orders_users_controller.rb
@@ -2,19 +2,23 @@ class Api::V1::ExecutiveOrdersUsersController < ApplicationController
   def create
     executive_order_number = params[:id] # might change to :document_number
 
-    executive_order = ExecutiveOrder.find_by(key: executive_order_number)
+    user = User.find_by(:user_id)
+
+    executive_order = ExecutiveOrder.find_by(executive_order_number)
     
     if executive_order === nil 
      found_executive_order = ExecutiveOrderDetailGateway.find_specific_eo(executive_order_number)
      ExecutiveOrder.create!(found_executive_order)
     end
 
-    executive_order_user
+    executive_order_user = Executive_Orders_Users.create!(executive_orders_users_params)
+
+    render json: executive_order_user
   end
 
   private 
 
-  def executive_order_params
-    params.permit(:title, :html_url, :executive_order_number, :publication_date)
+  def executive_orders_users_params
+    params.permit(:id, :executive_order_number)
   end
 end
