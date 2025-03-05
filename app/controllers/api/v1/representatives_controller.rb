f8c14a59ad6336443b9c2248b6382317075d9a2e
Jonathan Atkins <jonathanjatkins89@gmail.com>
2025-03-05 10:18:00 -0800

    updating representatives controller show

--- app/controllers/api/v1/representatives_controller.rb
+++ app/controllers/api/v1/representatives_controller.rb
@@ -1,23 +1,23 @@
 class Api::V1::RepresentativesController < ApplicationController
   def index
+    require 'pry'; binding.pry
     if params[:db].present? && params[:db] == true
       # For db representatives 
     else
       representatives = RepresentativeGateway.fetch_queried_reps(params[:query])
-      session[:reps] = representatives
-      # if params[:id].present?
-      #   representative = RepresentativePoro.find_by_id(params[:id], representatives)
-      #   render json: RepresentativeSerializer.new(representative)
-      # else
+      
       render json: RepresentativeSerializer.new(representatives)
-      # end
     end
   end
     
   def show
-    binding.pry
-    representatives = session[:reps]
-    representative = RepresentativePoro.find_by_id(params[:id], representatives)
-    render json: RepresentativeSerializer.new(representative)
+    if params[:db].present? && params[:db] == true
+      #For db representatives
+    else
+      representatives = RepresentativeGateway.fetch_queried_reps(params[:query])
+      representative  = RepresentativePoro.find_by_id(params[:id], representatives)
+  
+      render json: RepresentativeSerializer.new(representative)
+    end
   end
 end
