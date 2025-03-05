16617df805870705e8a662dd2995f538a3aa2af7
Kevin Newland <kevin.newland@yahoo.com>
2025-03-05 11:30:02 -0600

    created create action

--- app/controllers/api/v1/executive_orders_users_controller.rb
+++ app/controllers/api/v1/executive_orders_users_controller.rb
@@ -1,7 +1,20 @@
 class Api::V1::ExecutiveOrdersUsersController < ApplicationController
   def create
-    executive_order = params[:id]
+    executive_order_number = params[:id] # might change to :document_number
 
-    if executive_order
+    executive_order = ExecutiveOrder.find_by(key: executive_order_number)
+    
+    if executive_order === nil 
+     found_executive_order = ExecutiveOrderDetailGateway.find_specific_eo(executive_order_number)
+     ExecutiveOrder.create!(found_executive_order)
+    end
+
+    executive_order_user
+  end
+
+  private 
+
+  def executive_order_params
+    params.permit(:title, :html_url, :executive_order_number, :publication_date)
   end
 end
