51385b0fb24afe6dfd922ce59ddfb3e21a95027a
Jillian Bleggi <jillian.bleggi@gmail.com>
2025-03-07 17:52:32 -0500

    Update to ensure seeds are cleared and data is unique.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,16 @@
+# db/seeds.rb
+# 
 require 'faker'
 
 # user1 = User.create!(email: "funtimes@wtf.com", state: "Canada", zip: "11111")
+User.delete_all
 
 10.times do 
   User.create( 
-    email: Faker::Internet.email,
+    email: Faker::Internet.unique.email,
     state: Faker::Address.state,
     zip: Faker::Number.number(digits: 5) 
   )
 end
+
+Faker::UniqueGenerator.clear
