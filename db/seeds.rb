53ab7f3fd588274f00d7a96b9a246b2cae2179a2
Jillian Bleggi <jillian.bleggi@gmail.com>
2025-03-07 17:49:47 -0500

    Added 10 users to seed data using faker.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'faker'
 
-user1 = User.create!(email: "funtimes@wtf.com", state: "Canada", zip: "11111")
+# user1 = User.create!(email: "funtimes@wtf.com", state: "Canada", zip: "11111")
+
+10.times do 
+  User.create( 
+    email: Faker::Internet.email,
+    state: Faker::Address.state,
+    zip: Faker::Number.number(digits: 5) 
+  )
+end
