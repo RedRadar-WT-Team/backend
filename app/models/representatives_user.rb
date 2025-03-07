14fb48dfb48a2deab943e0dedb7607abe491a5a3
Kristin Weiland <kristinweiland@gmail.com>
2025-03-07 10:42:32 -0800

    Change scope to scope: :user_id

--- app/models/representatives_user.rb
+++ app/models/representatives_user.rb
@@ -2,5 +2,5 @@ class RepresentativesUser < ApplicationRecord
   belongs_to :representative
   belongs_to :user
 
-  validates :representative_id, uniqueness: { scope: :executive_order_id }
+  validates :representative_id, uniqueness: { scope: :user_id }
 end
