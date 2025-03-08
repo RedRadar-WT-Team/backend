e0e7e295983fe635bf78b5a537e0f6c4b98e58fc
Jillian Bleggi <jillian.bleggi@gmail.com>
2025-03-08 09:47:30 -0500

    Got GetUserProfile request spec to pass.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -40,6 +40,9 @@ rescue ActiveRecord::PendingMigrationError => e
   abort e.to_s.strip
 end
 RSpec.configure do |config|
+  config.before(:each, type: :request) do
+    host! 'localhost:3000'
+  end
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_paths = [
     Rails.root.join('spec/fixtures')
--- spec/requests/api/v1/get_user_profile_request.rb
+++ spec/requests/api/v1/get_user_profile_request.rb
@@ -1,14 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe 'GET /api/v1/profile', type: :request do
-  let(:header) { { 'Content-Type' => 'application/json', 'X-User-Id' => user.id.to_s } }
-
   it 'returns the current user profile' do 
-    get user_profile_path, headers: header
+    user = create(:user)
+    get "/api/v1/users/show?email=#{user.email}"
+  
+    expect(response).to have_http_status(200)
+
     json = JSON.parse(response.body, symbolize_names: true)
 
-
-    expect(repsonse).to have_http_status(200)
-    binding.pry
+    expect(json[:data][:attributes][:id]).to eq(user.id)
+    expect(json[:data][:attributes][:email]).to eq(user.email)
+    expect(json[:data][:attributes][:state]).to eq(user.state)
+    expect(json[:data][:attributes][:zip]).to eq(user.zip)
   end
 end
