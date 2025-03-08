f76fc43a7094b78a2db048f57a486aafb8350afe
Jillian Bleggi <jillian.bleggi@gmail.com>
2025-03-07 22:16:52 -0500

    User login works

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,17 @@
 # db/seeds.rb
 # 
-require 'faker'
+# require 'faker'
 
-# user1 = User.create!(email: "funtimes@wtf.com", state: "Canada", zip: "11111")
-User.delete_all
+User.create!(email: "funtimes@consultancy.com", state: "Maryland", zip: "20879")
 
-10.times do 
-  User.create( 
-    email: Faker::Internet.unique.email,
-    state: Faker::Address.state,
-    zip: Faker::Number.number(digits: 5) 
-  )
-end
+# User.delete_all
 
-Faker::UniqueGenerator.clear
+# 10.times do 
+#   User.create( 
+#     email: Faker::Internet.unique.email,
+#     state: Faker::Address.state,
+#     zip: Faker::Number.number(digits: 5) 
+#   )
+# end
+
+# Faker::UniqueGenerator.clear
