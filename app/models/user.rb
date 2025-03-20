36e3589028ffa3b9ad39990b5e5cdfcfb8e53dcc
Jillian Bleggi <jillian.bleggi@gmail.com>
2025-03-20 13:52:17 -0400

    Added user relationship with rep_users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,9 @@
 
 class User < ApplicationRecord
   validates_with UserValidator
+
+  has_many :representatives_users
+  has_many :representatives, through: :representatives_users
   
   has_many :executive_orders_users
   has_many :executive_orders, through: :executive_orders_users
