58fd5c7647ca42b51d0191727e583b476a527df1
Kristin Weiland <kristinweiland@gmail.com>
2025-03-25 14:42:41 -0700

    Add summary attribute to EO table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_03_19_232041) do
+ActiveRecord::Schema[7.1].define(version: 2025_03_25_200519) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_19_232041) do
     t.datetime "publication_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "summary"
   end
 
   create_table "executive_orders_users", force: :cascade do |t|
