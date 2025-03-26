a1fb4861afbba843ea9794e98094d99ae87632cc
Kristin Weiland <kristinweiland@gmail.com>
2025-03-25 21:36:56 -0700

    Debug ExecutiveOrdersUser fetch calls from FE.

--- app/controllers/api/v1/executive_orders_users_controller.rb
+++ app/controllers/api/v1/executive_orders_users_controller.rb
@@ -1,13 +1,23 @@
 class Api::V1::ExecutiveOrdersUsersController < ApplicationController
+  # before_action :set_user, only: [:create, :index, :destroy]
+
   def create
     executive_order_number = params[:executive_order_number]
-
-    user = User.find(session[:user_id])
-    executive_order = ExecutiveOrder.find_or_create_from_gateway(executive_order_number: executive_order_number)
     
-    executive_order_user = ExecutiveOrdersUser.create!(user: user, executive_order: executive_order)
+    user = User.find(session[:user_id])
 
-    render json: executive_order_user, status: :created
+    executive_order = ExecutiveOrder.find_or_create_from_gateway(executive_order_number: executive_order_number)
+
+    existing_association = ExecutiveOrdersUser.find_by(user: @user, executive_order: executive_order)
+
+    if existing_association
+      render json: { message: "Executive order already saved." }, status: :ok
+      return
+    end
+    
+    executive_order_user = ExecutiveOrdersUser.new(user: user, executive_order: executive_order)
+
+    render json: { message: "Successfully saved." }, status: :created
   end
 
   def index
@@ -39,4 +49,13 @@ class Api::V1::ExecutiveOrdersUsersController < ApplicationController
   def executive_orders_users_params
     params.permit(:user_id, :executive_order_id)
   end
+  
+  # def set_user
+  #   @user = User.find_by(id: session[:user_id])
+
+  #   if @user.nil?
+  #     render json: { error: 'User not found' }, status: :not_found
+  #   end
+  # end
+
 end
