ecd1d3a600a8d761effc2b1b61f0130e0ecf494d
Kristin Weiland <kristinweiland@gmail.com>
2025-03-25 17:30:57 -0700

    Make sure the summary is added to the EO poro.

--- app/gateways/executive_order_detail_gateway.rb
+++ app/gateways/executive_order_detail_gateway.rb
@@ -3,17 +3,17 @@ class ExecutiveOrderDetailGateway
   def self.find_specific_eo(document_number)
     Rails.cache.fetch("find_specific_eo_#{document_number}", expires_in: 12.hours) do
       selected_executive_order = hit_endpoint("api/v1/documents/#{document_number}.json")
-      eo_poro = ExecutiveOrderPoro.new(selected_executive_order)
       
       # Generate summary if EO exists in DB but doesn't have a summary
       if (eo = ExecutiveOrder.find_by(executive_order_number: document_number))
-        if !eo.summary.present? && eo_poro.pdf_url.present?
-          text = PdfExtractionService.extract_text(eo_poro.pdf_url)
+        if !eo.summary.present? && selected_executive_order[:pdf_url].present?
+          text = PdfExtractionService.extract_text(selected_executive_order[:pdf_url])
           eo.generate_and_save_summary(text) if text.present?
         end
+        selected_executive_order[:summary] = eo.summary
       end
       
-      eo_poro
+      ExecutiveOrderPoro.new(selected_executive_order)
     end
   end
 
